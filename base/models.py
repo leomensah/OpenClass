05eaf1728c0b59fb2efc9331487f9c49966c38c0
leomensah <leomensah.main@gmail.com>
2023-03-05 21:45:05 +0000

    user model updated

--- base/models.py
+++ base/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
+# Added User model for backwards compatibility
 class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
